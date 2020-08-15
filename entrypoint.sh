e54e846bd7dde334ca369923ae17f8d0013a39b7
Brian Douglas <bdougie@users.noreply.github.com>
2020-08-14 22:13:54 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-echo "Debug information:"
+if [[ "${{ github.event.comment.body }}" == ".take" ]]; then
+    echo "Assigning issue ${{ github.event.issue.number }} to ${{ github.event.comment.user.login }}"
+    curl -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" -d '{"assignees": ["${{ github.event.comment.user.login }}"]}' https://api.github.com/repos/${{ github.repository }}/issues/${{ github.event.issue.number }}/assignees
+fi
