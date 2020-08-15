9d2a9bd20607ab0ab71e822937b51d1c1e7fba30
Brian Douglas <bdougie@users.noreply.github.com>
2020-08-15 06:00:47 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-echo "---"
-echo "Event:"
-# jq . $GITHUB_EVENT_PATH
-
 BODY="$(jq '.comment.body' $GITHUB_EVENT_PATH)"
 ISSUE_NUMBER="$(jq '.issue.number' $GITHUB_EVENT_PATH)"
 LOGIN="$(jq '.comment.user.login' $GITHUB_EVENT_PATH)"
 REPO="$(jq '.repository.full_name' $GITHUB_EVENT_PATH)"
 
-# jq . $GITHUB_EVENT_PATH
-echo $BODY
-echo $LOGIN
-echo $ISSUE_NUMBER
-echo $REPO
+if [[ "$BODY" == ".take" ]]; then
+    echo "Assigning issue $ISSUE_NUMBER to $LOGIN"
+    curl -H "Authorization: token $GITHUB_TOKEN" -d "{'assignees':
+    ["$LOGIN"]}" https://api.github.com/repos/$REPO/issues/$ISSUE_NUMBER/assignees
+fi
