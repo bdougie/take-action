80600b0e93fd8f51777d915d7bfd3b0e43a9dddc
Brian Douglas <bdougie@users.noreply.github.com>
2020-09-19 23:32:53 -0700

    fixes the silent failure

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 BODY="$(jq '.comment.body' $GITHUB_EVENT_PATH)"
-ISSUE_NUMBER="$(jq '.issue.number' $GITHUB_EVENT_PATH)"
+ISSUE_NUMBER="$(jq '.issue.number' $GITHUB_EVENT_PATH | tr -d \")"
 LOGIN="$(jq '.comment.user.login' $GITHUB_EVENT_PATH | tr -d \")"
 REPO="$(jq '.repository.full_name' $GITHUB_EVENT_PATH | tr -d \")"
 
-if [[ "$BODY" == *".take"* ]]; then
+jq . $GITHUB_EVENT_PATH
+
+if [[ $BODY == *".take"* ]]; then
   echo "Assigning issue $ISSUE_NUMBER to $LOGIN"
+  echo "Using the link: https://api.github.com/repos/$REPO/issues/$ISSUE_NUMBER/assignees"
   curl -H "Authorization: token $GITHUB_TOKEN" -d '{"assignees":["'"$LOGIN"'"]}' https://api.github.com/repos/$REPO/issues/$ISSUE_NUMBER/assignees
 fi
